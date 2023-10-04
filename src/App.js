02715294b80939fb09e9d4c00e386aed4a50aab4
KenAuguis <auguiskenneth15@gmail.com>
2023-10-04 22:46:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,81 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function CalcButton({label}) {
+function CalcButton({ label, onClick }) {
   return (
-    <button>
+    <button onClick={() => onClick(label)}>
       {label}
     </button>
   );
 }
 
-function CalcDisplay({display}) {
+function CalcDisplay({ display }) {
   return (
     <div className='CalcDisplay'>
       {display}
     </div>
   );
 }
-function App() {
-  return(
-    <div className='CalcContainer'>
-      <CalcDisplay display={"Hello World"}/>
-    <div className='ButtonContainer'>
-      <CalcButton label={7}/>
-      <CalcButton label={8}/>
-      <CalcButton label={9}/>
-      <CalcButton label={"+"}/>
-      <CalcButton label={4}/>
-      <CalcButton label={5}/>
-      <CalcButton label={6}/>
-      <CalcButton label={"-"}/>
-      <CalcButton label={1}/>
-      <CalcButton label={2}/>
-      <CalcButton label={3}/>
-      <CalcButton label={"*"}/>
-      <CalcButton label={"C"}/>
-      <CalcButton label={0}/>
-      <CalcButton label={"="}/>
-      <CalcButton label={"/"}/>
 
-    </div> 
+function App() {
+  const [displayValue, setDisplayValue] = useState('');
+  const [calculation, setCalculation] = useState('');
+
+  const handleButtonClick = (buttonLabel) => {
+    if (buttonLabel === '=') {
+      try {
+        
+        const result = eval(calculation);
+        setDisplayValue(result.toString());
+        setCalculation(result.toString());
+      } catch (error) {
+        
+        setDisplayValue('Error');
+        setCalculation('');
+      }
+    } else if (buttonLabel === 'C') {
+      
+      setDisplayValue('');
+      setCalculation('');
+    } else {
+    
+      setCalculation((prevCalculation) => prevCalculation + buttonLabel);
+      setDisplayValue((prevDisplayValue) => prevDisplayValue + buttonLabel);
+    }
+  };
+  const handleSurnameButtonClick = () => {
+    // Add your full name to the display
+    setDisplayValue('Kenneth Clark G. Auguis');
+  };
+
+
+  return (
+    <div className='CalcContainer'>
+ <h1 className='CalculatorTitle'>Calculator of Kenneth Clark G. Auguis - IT3A</h1>
+      <CalcDisplay display={displayValue} />
+      <div className='ButtonContainer'>
+        <CalcButton label={7} onClick={handleButtonClick} />
+        <CalcButton label={8} onClick={handleButtonClick} />
+        <CalcButton label={9} onClick={handleButtonClick} />
+        <CalcButton label="+" onClick={handleButtonClick} />
+        <CalcButton label={4} onClick={handleButtonClick} />
+        <CalcButton label={5} onClick={handleButtonClick} />
+        <CalcButton label={6} onClick={handleButtonClick} />
+        <CalcButton label="-" onClick={handleButtonClick} />
+        <CalcButton label={1} onClick={handleButtonClick} />
+        <CalcButton label={2} onClick={handleButtonClick} />
+        <CalcButton label={3} onClick={handleButtonClick} />
+        <CalcButton label="*" onClick={handleButtonClick} />
+        <CalcButton label="C" onClick={handleButtonClick} />
+        <CalcButton label={0} onClick={handleButtonClick} />
+        <CalcButton label="=" onClick={handleButtonClick} />
+        <CalcButton label="/" onClick={handleButtonClick} />
+
+         {/* Surname button */}
+         <div className='Surname'>
+         <CalcButton label="AUGUIS" onClick={handleSurnameButtonClick} />
+         </div>
+      </div>
     </div>
   );
 }
